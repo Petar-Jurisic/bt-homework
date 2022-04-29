3b19ada2fcab85a60e7892e6add769a52cd0d8b4
Petar-Jurisic <jurisicpetar@gmail.com>
2022-04-29 16:37:27 +0200

    hw10 na drugi nacin

--- hw10.php
+++ hw10.php
@@ -1,53 +1,33 @@
 <html>
 
 <head>
-    <title>Homework 10</title>
+    <title>Homework 10-11</title>
 </head>
 
 <body>
     <?php
-    echo '<span style="font-size: 24px; color:purple">' . "<u>" . "Zadatak 1" . "</u>" . '</span>' . "<br>";
-    $a = 200;
+    echo '<span style="font-size: 24px; color:darkseagreen">' . "<u>" . "Zadatak 1" . "</u>" . '</span>' . "<br>";
+    
+	$a = 200;
     $b = 80;
-    echo ("a = " . $a) . "<br>";
+	echo ("a = " . $a) . "<br>";
     echo ("b = " . $b) . "<br>";
-    echo ("Zbir a i b je: " . ($a + $b)) . "<br>";
-    echo ("Proizvod a i b je: " . ($a * $b)) . "<br>";
-    echo ("Razlika a i b je: " . ($a - $b)) . "<br>";
-    echo ("Količnik a i b je: " . ($a / $b)) . "<br>" . "<br>";
-
-    echo '<span style="font-size: 24px; color:purple">' . "<u>" . "Zadatak 2" . "</u>" . '</span>' . "<br>" ;
+	echo ("Zbir a i b je: " . bcadd($a, $b)) . "<br>";
+	echo ("Proizvod a i b je: " . bcmul($a, $b)) . "<br>";
+	echo ("Razlika a i b je: " . bcsub($a, $b)) . "<br>";
+	echo ("Količnik a i b je: " . bcdiv($a, $b)) . "<br>" . "<br>";
+	
+    echo '<span style="font-size: 24px; color:darkseagreen">' . "<u>" . "Zadatak 2" . "</u>" . '</span>' . "<br>" ;
     $c = rand(0, 6);
-    switch ($c) {
-        case "0":
-            $c = "nedelja";
-            break;
-        case "1":
-            $c = "ponedeljak";
-            break;
-        case "2":
-            $c = "utorak";
-            break;
-        case "3":
-            $c = "sreda";
-            break;
-        case "4":
-            $c = "četvrtak";
-            break;
-        case "5":
-            $c = "petak";
-            break;
-        case "6":
-            $c = "subota";
-            break;
-    }
-    echo ("Danas je" . " " . $c . "!") . "<br>" . "<br>";
-
-    echo '<span style="font-size: 24px; color:purple">' . "<u>" . "Zadatak 3" . "</u>" . '</span>' . "<br>";
+	$days = ["nedelja", "ponedeljak", "utorak", "sreda", "četvrtak", "petak", "subota"];
+	echo ("Danas je" . " " . $days[$c] . "!") . "<br>" . "<br>";
+	
+    echo '<span style="font-size: 24px; color:darkseagreen">' . "<u>" . "Zadatak 3" . "</u>" . '</span>' . "<br>";
     $d = rand(0, 10);
     $e = rand(0, 10);
     $f = rand(0, 10);
-    echo "Zbir brojeva " . $d . ", " . $e . " i " . $f . " je " . ($d + $e + $f) . ".";
+	$niz = [$d, $e, $f];
+    echo "Zbir brojeva " . $d . ", " . $e . " i " . $f . " je " . array_sum($niz) . ".";
     ?>
 
 
